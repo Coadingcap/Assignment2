59b0bfff47fd2f1f79485110473b6e29e3d2a67f
Coadingcap <pspatil94235@gmail.com>
2022-04-21 10:00:27 +0530

    javascript

---
+++ array.js
@@ -0,0 +1,6 @@
+function createarray(){
+var languages=["HTML","CSS","Javascript","Python"];  
+for (i=0;i<languages.length;i++){ 
+document.write("languages["+i+"] :"+languages[i] + "<br/>");  
+}  
+}
---
+++ events..js
@@ -0,0 +1,11 @@
+function changebgcolor1(){
+document.body.style.backgroundColor = "lightgreen";
+}
+
+function changebgcolor2(){
+document.body.style.backgroundColor = "aqua";
+}
+
+function welcome(){
+alert("Welcomeee To JS Events");
+}
